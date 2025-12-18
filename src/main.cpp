2d34aedd278dc7a49d85d45f6ae51ce7ff0fec0f
torkel <184108183+torkelicious@users.noreply.github.com>
2025-12-18 12:54:55 +0100

    install scripts and command line args to show window

--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,52 @@
-#include <QApplication>
-#include <QLockFile>
-#include <QDir>
-#include <QMessageBox>
 #include "mainfunctions.h"
 #include "systray.h"
 #include "widget.h"
+#include <QApplication>
+#include <QCommandLineParser>
+#include <QDir>
+#include <QLockFile>
+#include <QMessageBox>
 
 // TODO: implement config menu for mode / account type / registration
 // TODO: config for auto start & autoconnect
-// TODO:Accept cli args to show the widget instead of hiding it
+// DONE: Accept cli args to show the widget instead of hiding it
 
 int main(int argc, char *argv[]) {
-    QApplication a(argc, argv);
+  QApplication a(argc, argv);
+  QApplication::setApplicationName("CloudflareWarpQt");
 
-    // Single Instance Lock
-    QLockFile lockFile(QDir::temp().absoluteFilePath("warp-qt.lock"));
+  // Single Instance Lock
+  QLockFile lockFile(QDir::temp().absoluteFilePath("warp-qt.lock"));
 
-    // If returns false, another instance is running.
-    if (!lockFile.tryLock(100)) {
-        QMessageBox::warning(nullptr,
-                             "WarpQt",
-                             "The application is already running!\nCheck your system tray.");
-        return 1;
-    }
+  // If returns false, another instance is running.
+  if (!lockFile.tryLock(100)) {
+    QMessageBox::warning(
+        nullptr, "WarpQt",
+        "The application is already running!\nCheck your system tray.");
+    return 1;
+  }
 
-    Widget w;
+  QCommandLineParser parser;
+  parser.setApplicationDescription("Qt6 GUI for Cloudflare Warp");
+  parser.addHelpOption();
+  parser.addVersionOption();
 
-    // tray apps should start hidden...? i think...
-    // w.show();
+  QCommandLineOption showOption("show", "Start with the window visible.");
+  parser.addOption(showOption);
+  parser.process(a);
 
-    SysTray tray(&w);
+  Widget w;
 
-    QObject::connect(&tray, &SysTray::connectionChanged, &w, &Widget::onConnectionChanged);
-    QObject::connect(&w, &Widget::connectionChanged, &tray, &SysTray::updateStatus);
+  SysTray tray(&w);
 
-    return a.exec();
+  QObject::connect(&tray, &SysTray::connectionChanged, &w,
+                   &Widget::onConnectionChanged);
+  QObject::connect(&w, &Widget::connectionChanged, &tray,
+                   &SysTray::updateStatus);
+
+  if (parser.isSet(showOption)) {
+    w.show();
+  }
+
+  return a.exec();
 }
